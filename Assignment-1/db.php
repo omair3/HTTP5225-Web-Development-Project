cb59700babd127a1761c6aeebb5c13e1d4ce574d
omair3 <mohdomair3@outlook.com>
2025-08-14 19:51:12 -0400

    Corrected Laravel Student CRUD files

--- Assignment-1/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
- $conn = mysqli_connect('127.0.0.1', 'root', '', 'assignment-1', 3307);
-//$conn = new mysqli($host, $user, $pass, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
--- Week7/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
- // $connect = mysqli_connect('localhost', 'root', '', 'schools');
-  $connect = mysqli_connect('127.0.0.1', 'root', '', 'schools', 3307);
-  if(!$connect){
-    die("Connection Failed: " . mysqli_connect_error());
-  }
-?>
